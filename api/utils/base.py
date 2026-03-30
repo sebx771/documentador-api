80e1c59ecd54a833ad48f65da9db56e0b3b84fc5
Santiago <sebascova18@gmail.com>
2026-03-30 18:37:46 -0500

    feat(utils): hace la funcion base64 a string mas flexible

--- api/utils/base.py
+++ api/utils/base.py
@@ -4,7 +4,11 @@ import base64
 def base64_to_string(b64):
 
     try:
-        bytes_data = b64.encode('utf-8')
+        if isinstance(b64, str):
+         bytes_data = b64.encode('utf-8')
+        else:
+            bytes_data = b64
+            
         decoded_bytes = base64.b64decode(bytes_data)
         return decoded_bytes.decode('utf-8')
     except Exception as e:
