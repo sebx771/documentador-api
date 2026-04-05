1e60afad1029b89c29844e9a6b6598472c6ccd4e
Sebastian <sebascova18@gmail.com>
2026-04-05 13:04:21 -0500

    feat(utils): añade validacion a la funcion preparar descargas para que acepte bytes y strings

--- api/utils/bytes_utils.py
+++ api/utils/bytes_utils.py
@@ -2,6 +2,8 @@ import io
 
 def preparar_descarga(contenido):
     buf = io.BytesIO()
-    buf.write(contenido.encode('utf-8'))
+    if isinstance(contenido, str):
+        contenido = contenido.encode('utf-8')
+    buf.write(contenido)
     buf.seek(0)  # Volvemos al inicio del "archivo" virtual
     return buf
