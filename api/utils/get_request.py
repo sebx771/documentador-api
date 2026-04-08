6d29aba5a3e110813d29a97334158d2819cddd60
Sebastian <sebascova18@gmail.com>
2026-04-08 14:31:03 -0500

    refactor(utils): mejora validacion en funcion auxiliar para mayor robustez

--- api/utils/get_request.py
+++ api/utils/get_request.py
@@ -1,14 +1,14 @@
 
 
 def get_request_data(request):
-    if request.files:
+    if request.files and "file" in request.files:
        file= request.files["file"]
        extra= request.form.get("extra", "")
-       return file, extra
-    elif request.json:
+       return file, extra or ""
+    elif request.is_json:
         data= request.get_json()
-        extra= data.get("extra", "")
-        return data, extra
+        extra= data.get("extra", "") if data else ""
+        return data, (extra or "")
     else:
         return None, None
 
