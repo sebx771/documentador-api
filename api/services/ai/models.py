461d663eb2d0b548be571611531ef0c4d9e7e007
Sebastian <sebascova18@gmail.com>
2026-04-20 19:52:44 -0500

    feat(ai): añade archivo con diccionario que incluye los modelos a usar y su funcion para mayor orden

---
+++ api/services/ai/models.py
@@ -0,0 +1,20 @@
+
+
+models = {
+    "chunking": {
+        "id": "llama-3.1-8b-instant",
+        "tpm": 14400
+    },
+    "final_doc": {
+        "id": "openai/gpt-oss-120b",
+        "tpm": 8000
+    },
+    "fallback": {
+        "id": "qwen/qwen3-32b",
+        "tpm": 6000
+    },
+    "emergency": {
+        "id": "openai/gpt-oss-20b",
+        "tpm": 8000
+    }
+}
