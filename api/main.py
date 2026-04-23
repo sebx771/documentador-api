34f5a93fddc8144c9f64ee8b6c0736effbb6f0f2
Sebastian <sebascova18@gmail.com>
2026-04-22 19:56:56 -0500

    chore(main): actualiza api version

--- api/main.py
+++ api/main.py
@@ -11,7 +11,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-API_VERSION = "2.2.0"
+API_VERSION = "2.3.0"
 app = Flask(__name__)
 CORS(app)
 app.json.sort_keys = False # esto hace que el JSON tenga el mismo orden que las claves de diccionarios
