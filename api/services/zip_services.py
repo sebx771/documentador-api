790a74d7bac74d94ce9c156db2529b20417872fb
Sebastian <sebascova18@gmail.com>
2026-05-12 16:41:45 -0500

    refactor(services): remueve test unitario del servicio zip

--- api/services/zip_services.py
+++ api/services/zip_services.py
@@ -5,54 +5,78 @@ import os
 import time
 
 
-class ZipService():
+class ZipService:
     def __init__(self):
-        self.allowed_extensions = {".py", ".java", ".go", ".js", ".ts",".php",".css", ".html", ".json", ".xml", ".yml", ".yaml"}
-        self.ignore_folders= {"node_modules",".git",".gitignore",".DS_Store","__pycache__",".venv"}
-        self.ignore_files= {"package-lock.json", "yarn.lock"}
+        self.allowed_extensions = {
+            ".py",
+            ".java",
+            ".go",
+            ".js",
+            ".ts",
+            ".php",
+            ".css",
+            ".html",
+            ".json",
+            ".xml",
+            ".yml",
+            ".yaml",
+        }
+        self.ignore_folders = {
+            "node_modules",
+            ".git",
+            ".gitignore",
+            ".DS_Store",
+            "__pycache__",
+            ".venv",
+        }
+        self.ignore_files = {"package-lock.json", "yarn.lock"}
 
     def extraer_zip(self, contenido_bytes: bytes):
-        MAX_FILES= 50
-        codigo_total= []
-        codigo_invalido= []
-             
+        MAX_FILES = 50
+        codigo_total = []
+        codigo_invalido = []
+
         try:
             with zipfile.ZipFile(io.BytesIO(contenido_bytes)) as zip_file:
                 for file in zip_file.namelist():
-                    _,ext= os.path.splitext(file.lower())
+                    _, ext = os.path.splitext(file.lower())
 
                     if file.endswith("/"):
                         continue
 
                     if any(folder in file for folder in self.ignore_folders):
-                     continue
+                        continue
 
                     if any(file.endswith(ignore) for ignore in self.ignore_files):
-                      continue
+                        continue
 
                     if ext in self.allowed_extensions:
                         try:
                             with zip_file.open(file) as f:
-                                contenido= f.read().decode('utf-8',errors="ignore")
+                                contenido = f.read().decode("utf-8", errors="ignore")
                                 if contenido.strip():
-                                 codigo_total.append(f"\n\n### Archivo:{file}\nLenguaje: {ext.replace('.', '')}\n\n{contenido}")
+                                    codigo_total.append(
+                                        f"\n\n### Archivo:{file}\nLenguaje: {ext.replace('.', '')}\n\n{contenido}"
+                                    )
 
-                                 if len(codigo_total) >= MAX_FILES:
-                                    break 
+                                    if len(codigo_total) >= MAX_FILES:
+                                        break
                         except Exception as e:
-                            codigo_invalido.append(f"Error al leer el archivo {file}: {str(e)}")
+                            codigo_invalido.append(
+                                f"Error al leer el archivo {file}: {str(e)}"
+                            )
                             continue
         except zipfile.BadZipFile as e:
             print(f"Error al extraer el ZIP: {str(e)}")
             raise Exception("Archivo ZIP inválido o corrupto")
-        
-        return "\n".join(codigo_total) , codigo_invalido
+
+        return "\n".join(codigo_total), codigo_invalido
 
     def crear_zip(self):
         pass
 
-    def listar_contenido_zip(self, contenido_bytes:bytes)->list:
-        resultado= []
+    def listar_contenido_zip(self, contenido_bytes: bytes) -> list:
+        resultado = []
         try:
             with zipfile.ZipFile(io.BytesIO(contenido_bytes)) as zip_file:
                 for file in zip_file.namelist():
@@ -60,36 +84,25 @@ class ZipService():
                         continue
 
                     if any(folder in file for folder in self.ignore_folders):
-                     continue
+                        continue
 
                     if any(file.endswith(ignore) for ignore in self.ignore_files):
-                      continue
-                    
-                    _,ext= os.path.splitext(file.lower())
-                    is_valid= ext in self.allowed_extensions
-                    info= zip_file.getinfo(file)
+                        continue
 
-                    f= {
-                            "file":file,
-                            "language":ext.replace(".",""),
-                            "valid":is_valid,
-                            "size": f"{round(info.file_size/1024,2)}kb"
-                        }
+                    _, ext = os.path.splitext(file.lower())
+                    is_valid = ext in self.allowed_extensions
+                    info = zip_file.getinfo(file)
+
+                    f = {
+                        "file": file,
+                        "language": ext.replace(".", ""),
+                        "valid": is_valid,
+                        "size": f"{round(info.file_size/1024,2)}kb",
+                    }
                     resultado.append(f)
-            
+
         except zipfile.BadZipFile as e:
-         raise Exception("Archivo ZIP inválido o corrupto")
-        
+            raise Exception("Archivo ZIP inválido o corrupto")
+
         return resultado
-        
-#test
-if __name__ == "__main__":
-    zip= ZipService()
-    with open("ejercicios_javascript.zip", "rb") as f:
-        contenido_bytes= f.read()
-        codigo= zip.listar_contenido_zip(contenido_bytes)
-        for c in codigo:
-            time.sleep(2)
-            print(c)
-        
-        
+
